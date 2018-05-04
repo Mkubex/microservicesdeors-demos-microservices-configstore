0904b3db7e9a297146ade536f903bf4147e0e017
Gamez Albarran <a.gamez.albarran@accenture.com>
2018-05-04 16:16:57 +0200

    Commit2

---
+++ microservices/deors-demos-microservices-configservice/src/main/java/deors/demos/microservices/deorsdemosmicroservicesconfigservice/DeorsDemosMicroservicesConfigserviceApplication.java
@@ -0,0 +1,12 @@
+package deors.demos.microservices.deorsdemosmicroservicesconfigservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeorsDemosMicroservicesConfigserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeorsDemosMicroservicesConfigserviceApplication.class, args);
+	}
+}
